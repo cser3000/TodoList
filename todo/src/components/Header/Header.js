244e1f0bb63bae66fca57e97880f0e9cce60f97c
ryzhov.ik <cser3000@mail.ru>
2023-08-04 10:53:26 +0300

    #3 add components

---
+++ todo/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <form>
+                поиск
+            </form>
+            <button>
+                add
+            </button>
+        </header>
+    )
+}
+
+export default Header;
---
+++ todo/src/components/Todos/Todos.js
@@ -0,0 +1,11 @@
+import './Todos.css';
+
+function Todos() {
+    return (
+      <>
+        <h1>Задачи</h1>
+      </>
+    );
+}
+
+export default Todos;
